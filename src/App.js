5ea14d19bef82f8b2f3b9718b9d5ed4bd3316fd3
Natalia Budulchieva <nbudulchieva@gmail.com>
2023-05-11 20:00:07 +0200

    Create a rough html tree to visualize the page

--- src/App.js
+++ src/App.js
@@ -1,3 +1,28 @@
+// import styles from './App.module.scss';
+
 export default function App() {
-  return null;
+  return (
+    <>
+      <h1>Create a guest list for your event</h1>
+
+      <div>Add a guest using their first and last name</div>
+
+      <form>
+        <label>
+          First name
+          <input />
+        </label>
+        <label>
+          Last name
+          <input />
+        </label>
+      </form>
+
+      <div data-test-id="guest">
+        <div>Guest info here</div>
+        <input type="checkbox" aria-label="attending status" />
+        <button>Remove</button>
+      </div>
+    </>
+  );
 }
