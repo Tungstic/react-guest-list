8a3f2eb303f26f5a800949b0bf054b903322e63c
Natalia Budulchieva <nbudulchieva@gmail.com>
2023-05-11 21:04:00 +0200

    Try approach A for adding guest onKeyDown

--- src/App.js
+++ src/App.js
@@ -1,20 +1,65 @@
 // import styles from './App.module.scss';
+import { useState } from 'react';
 
 export default function App() {
+  // declare state variable
+  const [newGuest, setNewGuest] = useState({
+    firstName: '',
+    lastName: '',
+    attending: false,
+  });
+  /*
+  function clearInput(event) {
+    event.currentTarget.value = '';
+  } */
+
+  function addGuest(event) {
+    if (event.key === 'Enter') {
+      setNewGuest({
+        firstName: newGuest.firstName,
+        lastName: newGuest.lastName,
+        attending: newGuest.attending,
+      });
+    }
+    console.log(newGuest);
+    // clearInput();
+  }
+
   return (
     <>
       <h1>Create a guest list for your event</h1>
 
       <div>Add a guest using their first and last name</div>
 
-      <form>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+        }}
+      >
         <label>
           First name
-          <input />
+          <input
+            onChange={(event) =>
+              setNewGuest({
+                firstName: event.currentTarget.value,
+                lastName: newGuest.lastName,
+                attending: newGuest.attending,
+              })
+            }
+          />
         </label>
         <label>
           Last name
-          <input />
+          <input
+            onChange={(event) =>
+              setNewGuest({
+                firstName: newGuest.firstName,
+                lastName: event.currentTarget.value,
+                attending: newGuest.attending,
+              })
+            }
+            onKeyDown={addGuest}
+          />
         </label>
       </form>
 
